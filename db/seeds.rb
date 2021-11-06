66ba6a9c61cf7a3eb7495889b5180d459371fa78
ashokkadashetti <kadashettiashok@gmail.com>
2021-11-06 18:33:35 +0530

    Implemented custom scops for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+
+end
+
+puts "3 Topics created!"
+
+
 10.times do |blog|
 	Blog.create!(
-		title: "my Blog post#{blog}",
-		body: "This is body of blog post"
+		title: "My Blog post #{blog}",
+		body: "This is body of blog post",
+		topic_id: Topic.last.id
 	)
 end
 
@@ -16,10 +27,20 @@ end
 
 puts "5 skills cerated"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
-		title: "portfolio title#{portfolio_item}",
-		subtitle: "My great service",
+		title: "Portfolio title #{portfolio_item}",
+		subtitle: "Ruby on Rails",
+		body: "This is body of portfolio post",
+		main_image: "https://placehold.it/600x400",
+		thumb_image: "https://placehold.it/350x200"
+	)
+end
+
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title #{portfolio_item}",
+		subtitle: "Angular",
 		body: "This is body of portfolio post",
 		main_image: "https://placehold.it/600x400",
 		thumb_image: "https://placehold.it/350x200"
