4aa07f51ca2f13aec5c111df3c46d2330a5cd2aa
“ashokkadashetti” <kadashettiashok@gmail.com>
2021-11-20 13:01:50 +0530

    Added nav bar, and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,12 +5,15 @@
   # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
+    @page_title="My Portfolio Blog"
   end
 
 #..............................................................................
 
   # GET /blogs/1 or /blogs/1.json
   def show
+    @page_title=@blog.title
+    @seo_keywords=@blog.body
   end
 
 #..............................................................................
